950a5fedfe1ba428fc1bd5e151e9e4b56facad99
ocampco <ocampco@gmail.com>
2021-12-17 15:35:44 +1100

    Content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,59 @@
 import React from 'react';
+import styled from 'styled-components';
+import { colors, breakpoints } from './../styles/variables';
 import './../styles/index.css';
 
-const styles = {
-  hero: {
-    width: '100%',
-    height: '100vh',
-    backgroundColor: '#c4ded1',
-  },
-};
+const Hero = styled.section`
+  background-color: ${colors.lightGreen};
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  padding: 4rem 1rem;
+`;
+
+const HeroHeading = styled.h1`
+  font-size: 4rem;
+  font-weight: 600;
+  color: ${colors.white};
+  margin-bottom: 1.5rem;
+`;
+
+const HeroSubHeading = styled.address`
+  font-size: 1.5rem;
+  color: ${colors.white};
+`;
+
+const Background = styled.section`
+  padding: 2rem 1rem;
+`;
+
+const BackgroundHeading = styled.h2`
+  font-size: 1.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+const ListItem = styled.li`
+  font-size: 1.5rem;
+  font-weight: 300;
+  margin-bottom: 0.5rem;
+  color: ${colors.black};
+`;
 
 const IndexPage = () => (
   <main>
-    <div style={styles.hero}>
-      <h1>Face Studio By Annie</h1>
-    </div>
+    <Hero>
+      <HeroHeading>Face Studio<br />By Annie</HeroHeading>
+      <HeroSubHeading>Mildura, VIC</HeroSubHeading>
+    </Hero>
+    <Background>
+      <BackgroundHeading id="about">About</BackgroundHeading>
+      <ul>
+        <ListItem>9 years experience,</ListItem>
+        <ListItem>Certified make up artist,</ListItem>
+        <ListItem>Certified brow technician,</ListItem>
+        <ListItem>Face aesthetics specialist.</ListItem>
+      </ul>
+    </Background>
   </main>
 );
 
---
+++ src/styles/variables.js
@@ -0,0 +1,11 @@
+export const colors = {
+    lightGreen: '#c4ded1',
+    midGreen: '#a2d16e',
+    darkGreen: '#76ac53',
+    white: '#fff',
+    black: '#333f48',
+};
+
+export const breakpoints = {
+    medium: '(min-width: 480px)',
+};
