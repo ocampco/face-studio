1008d8ee4baa8b70db7d089e933bd8727e9f0b7a
ocampco <ocampco@gmail.com>
2022-06-28 17:46:30 +1000

    Placeholder contact style

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+import { StaticImage } from 'gatsby-plugin-image';
+import BookingLink from './BookingLink';
+import { colors } from './../styles/variables';
+
+const Section = styled.section`
+  padding: 2rem 0;
+`;
+
+const Heading = styled.h2`
+  margin-bottom: 1.5rem;
+`;
+
+const Username = styled.h3`
+  margin-bottom: 0.75rem;
+`;
+
+const Avatar = styled.div`
+  float: left;
+  width: 5rem;
+  margin-right: 1rem;
+  overflow: hidden;
+  border-radius: 50%;
+`;
+
+const Card = styled.div`
+  font-size: 1.25rem;
+  letter-spacing: 1px;
+  font-weight: 400;
+  margin-bottom: 0.75rem;
+  color: ${colors.black};
+`;
 
 const Contact = () => (
-  <section>
-    <h2 id="Contact">Contact me</h2>
-  </section>
+  <Section>
+    <Heading id="Contact">Contact me</Heading>
+    <Avatar>
+      <StaticImage
+        src="./../images/avatar.jpg"
+        alt="Annie"
+        placeholder="blurred"
+      />
+    </Avatar>
+    <Card>
+      <Username>facestudiobyannie</Username>
+      <BookingLink text="View on Instagram" />
+    </Card>
+  </Section>
 );
 
 export default Contact;
