85707679593966a535f756485e3b1eb743ec8ad7
ocampco <ocampco@gmail.com>
2022-06-30 19:00:39 +1000

    Use static review comment components since dynamic is not supported

---
+++ src/components/Comments.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import styled from 'styled-components';
+import { StaticImage } from 'gatsby-plugin-image';
+import { colors } from '../styles/variables';
+
+const Card = styled.div`
+  padding: 1.5rem;
+  margin-bottom: 1rem;
+  border: 1px solid ${colors.lightGrey};
+  border-radius: 0.5rem;
+`;
+
+const User = styled.div`
+  display: flex;
+  margin-bottom: 1.5rem;
+`;
+
+const Avatar = styled.div`
+  width: 5rem;
+  margin-right: 1rem;
+  overflow: hidden;
+  border-radius: 50%;
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 100%;
+`;
+
+const Username = styled.span`
+  font-weight: 500;
+`;
+
+const Source = styled.a`
+  color: ${colors.darkGrey};
+`;
+
+const Comment = styled.div`
+  line-height: 1.5;
+`;
+
+const Comments = () => (
+  <>
+    <Card>
+      <User>
+        <Avatar>
+          <StaticImage
+            src="./../images/review-meg.jpg"
+            alt="Meg"
+            placeholder="blurred"
+          />
+        </Avatar>
+        <Info>
+          <Username>Meg</Username>
+          <Source
+            href="https://www.facebook.com/meg.humphrey.31/posts/pfbid0mZ6pTAZFxW4oyzi4z5u9dtgz73arFvdWopuBuo7qESffmpEvoyj7NgyVt8p9sSSAl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Source
+          </Source>
+        </Info>
+      </User>
+      <Comment>
+        Absolutely brilliant! Annie really knows her stuff. Can’t wait to book in for everything!
+      </Comment>
+    </Card>
+    <Card>
+      <User>
+        <Avatar>
+          <StaticImage
+            src="./../images/review-whitts.jpg"
+            alt="Whitts"
+            placeholder="blurred"
+          />
+        </Avatar>
+        <Info>
+          <Username>Whitts</Username>
+          <Source
+            href="https://www.facebook.com/whitts.oliver/posts/pfbid03CRJG9o2HWHVnVyYudUkHDEZiENsMFu3w4Qg7ntm4j7PLRqRvTgVu94giV4Bdbdpl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Source
+          </Source>
+        </Info>
+      </User>
+      <Comment>
+        My eyebrow and lash queen!!
+      </Comment>
+    </Card>
+    <Card>
+      <User>
+        <Avatar>
+          <StaticImage
+            src="./../images/review-petra.jpg"
+            alt="Petra"
+            placeholder="blurred"
+          />
+        </Avatar>
+        <Info>
+          <Username>Petra</Username>
+          <Source
+            href="https://www.facebook.com/hejpetra101/posts/pfbid02CwuexLusx8YN4BEepbujuTf7rAyNG764EXmfGbQe1GcGo9tRT2RqM4uzsQrUC4g9l"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Source
+          </Source>
+        </Info>
+      </User>
+      <Comment>
+        Obsessed with the lashes Annie did for me. Beautiful lash extensions and I keep getting asked who did them. Thank you!
+      </Comment>
+    </Card>
+  </>
+);
+
+export default Comments;
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Review from './Review';
+import Comments from './Comments';
 import styled from 'styled-components';
 import config from '../config';
 
@@ -12,38 +12,12 @@ const Link = styled.a`
   margin-right: 0.25rem;
 `;
 
-const reviews = [{
-  image: './../images/review-meg.jpg',
-  name: 'Meg',
-  source: 'https://www.facebook.com/meg.humphrey.31/posts/pfbid0mZ6pTAZFxW4oyzi4z5u9dtgz73arFvdWopuBuo7qESffmpEvoyj7NgyVt8p9sSSAl',
-  comment: 'Absolutely brilliant! Annie really knows her stuff. Can’t wait to book in for everything!',
-}, {
-  image: './../images/review-whitts.jpg',
-  name: 'Whitts',
-  source: 'https://www.facebook.com/whitts.oliver/posts/pfbid03CRJG9o2HWHVnVyYudUkHDEZiENsMFu3w4Qg7ntm4j7PLRqRvTgVu94giV4Bdbdpl',
-  comment: 'My eyebrow and lash queen!!',
-}, {
-  image: './../images/review-petra.jpg',
-  name: 'Petra',
-  source: 'https://www.facebook.com/hejpetra101/posts/pfbid02CwuexLusx8YN4BEepbujuTf7rAyNG764EXmfGbQe1GcGo9tRT2RqM4uzsQrUC4g9l',
-  comment: 'Obsessed with the lashes Annie did for me. Beautiful lash extensions and I keep getting asked who did them. Thank you!',
-}]
-
-// TODO: Use shared link
+// TODO: Use shared link component
 const Reviews = () => (
   <section>
     <h2 id="Reviews">Reviews</h2>
     <Cards>
-    { reviews.map(
-        ({ image, name, source, comment }) =>
-          <Review
-            key={source}
-            image={image}
-            name={name}
-            source={source}
-            comment={comment}
-          />
-    )}
+      <Comments />
     </Cards>
     <Link
       href={config.FB_REVIEWS_URL}
